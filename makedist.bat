8e9176e75e1f77f677c4a166bd5988f839fb074e
Haymaker-1 <Haymaker-1@users.noreply.github.com>
2015-01-24 12:12:26 +0100

    added call to BinPBO to makedist bat file

--- makedist.bat
+++ makedist.bat
@@ -1,14 +1,16 @@
 
-rmdir /S /Q %CD%\..\tmp.Altis
 
-robocopy .\data .\..\tmp.Altis\data /NP /NDL /NFL /NJH /NJS /E
-robocopy .\functions .\..\tmp.Altis\functions /NP /NDL /NFL /NJH /NJS /E
-robocopy .\images\ .\..\tmp.Altis\images mission-wait-08.paa /NP /NDL /NFL /NJH /NJS /E
-robocopy .\scripts .\..\tmp.Altis\scripts /NP /NDL /NFL /NJH /NJS /E
-robocopy .\sounds .\..\tmp.Altis\sounds /NP /NDL /NFL /NJH /NJS /E
-robocopy .\ .\..\tmp.Altis\ description.ext init.sqf LICENSE mission.sqm  /NP /NDL /NFL /NJH /NJS
+set BINPBO_PROGRAM_PATH=%programfiles(x86)%\Bohemia Interactive\Tools\BinPBO Personal Edition\
+set thisDir=%cd%
 
+rmdir /S /Q %CD%\..\crimson-legacy.Altis
 
+robocopy .\data .\..\crimson-legacy.Altis\data /NP /NDL /NFL /NJH /NJS /E
+robocopy .\functions .\..\crimson-legacy.Altis\functions /NP /NDL /NFL /NJH /NJS /E
+robocopy .\images\ .\..\crimson-legacy.Altis\images mission-wait-08.paa /NP /NDL /NFL /NJH /NJS /E
+robocopy .\scripts .\..\crimson-legacy.Altis\scripts /NP /NDL /NFL /NJH /NJS /E
+robocopy .\sounds .\..\crimson-legacy.Altis\sounds /NP /NDL /NFL /NJH /NJS /E
+robocopy .\ .\..\crimson-legacy.Altis\ description.ext init.sqf LICENSE mission.sqm  /NP /NDL /NFL /NJH /NJS
 
-
+"%BINPBO_PROGRAM_PATH%\BinPBO.exe" "%thisDir%\..\crimson-legacy.Altis" "%thisDir%\.." -PACK -DEBUG
 
